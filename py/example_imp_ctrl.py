d88e0b5aff99be0dbd15a60f77b2e909177ac8b1
Matthias Kubisch <kubisch@informatik.hu-berlin.de>
2018-10-30 10:43:46 +0100

    improved example.

--- py/example_imp_ctrl.py
+++ py/example_imp_ctrl.py
@@ -1,13 +1,40 @@
 #!/usr/bin/python
 
+"""
+   +---------------------------------------------------------+
+   | Warning: This example demonstrates the impulse control  |
+   | method to be used with solenoids and should NOT be used |
+   | for servo motors with end-stops. ~~~~~~~~~~~~~~~~~~~~~~ |
+   +---------------------------------------------------------+
+"""
+
 from src.sensorimotor import Sensorimotor
 from time import sleep
 
 def print_position(data):
     print(''.join('{0: .2f} '.format(k) for k in data))
 
+
+tst0 = [ ([0.0, 0.6, 0.0], 0.20)
+       , ([0.0, 0.0, 0.6], 0.20)
+       , ([0.6, 0.0, 0.0], 0.20)
+       , ([0.0, 0.0, 0.6], 0.20)
+       , ([0.0, 0.6, 0.0], 0.10)
+       , ([0.0, 0.0, 0.6], 0.20)
+       , ([0.0, 0.6, 0.0], 0.10)
+       , ([0.6, 0.0, 0.0], 0.20)
+       , ([0.0, 0.0, 0.6], 0.20)
+       ]
+
+tst1 = [ ([0.0, 0.6, 0.0], 0.20)
+       , ([0.0, 0.0, 0.6], 0.20)
+       , ([0.6, 0.0, 0.0], 0.20)
+       , ([0.0, 0.0, 0.6], 0.20)
+       ]
+
+
 def main():
-    motors = Sensorimotor(number_of_motors = 2, verbose = False)
+    motors = Sensorimotor(number_of_motors = 3, verbose = False)
 
     # checking for motors
     N = motors.ping()
@@ -15,8 +42,10 @@ def main():
     sleep(1.0)
 
     # starting motorcord
-    motors.set_voltage_limit([0.5, 1.0])
+    motors.set_voltage_limit([0.6, 0.6, 0.6])
     motors.start()
+
+    beat = tst0
 
     try:
 
@@ -24,22 +53,11 @@ def main():
             # USER CODE HERE BEGIN
 
             print("tak")
-            motors.apply_impulse([0.5, 0.0])
-            sleep(0.4)
-
-            motors.apply_impulse([0.0, 1.0])
-            sleep(0.4)
-
-            motors.apply_impulse([0.5, 0.0])
-            sleep(0.2)
-            motors.apply_impulse([0.5, 0.0])
-            sleep(0.2)
-
-            motors.apply_impulse([0.0, 0.75])
-            sleep(0.4)
+            for b in beat:
+                motors.apply_impulse(b[0])
+                sleep(b[1]*1.2)
 
             # USER CODE HERE END
-
 
 
     except (KeyboardInterrupt, SystemExit):
