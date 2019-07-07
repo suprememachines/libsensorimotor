93ec1677988d12173df02b18eca5f2c04ceace54
Matthias Kubisch <kubisch@informatik.hu-berlin.de>
2019-07-07 23:26:33 +0100

    updated csl

--- src/controller/csl_control.hpp
+++ src/controller/csl_control.hpp
@@ -10,6 +10,11 @@ inline double pos(double value) { return std::max(.0, value); }
 inline double neg(double value) { return std::min(.0, value); }
 inline double posneg(double value, double p, double n) { return p*pos(value) + n*neg(value); }
 
+/**TODO:
+ + make this class easy to use for all
+ + remove update_mode from loop
+ + make safe to use, careful init, bounds etc.
+ */
 
 class csl_control {
 public:
@@ -20,28 +25,30 @@ public:
     double limit_lo        = -0.8;
     double limit_hi        = +0.8;
 
+    double gi_pos =  4.0;
+    double gi_neg = 16.0;
+
     double gi = .0;
     double gf = .0;
 
-    uint8_t id;
+    const uint8_t id;
+    const double dt;
 
-    csl_control(uint8_t id = 0) : id(id) { }
+    csl_control(uint8_t id = 0, double dt = .01/*100Hz*/) : id(id), dt(dt) { }
 
-    /* TODO make value gi configurable */
     void update_mode() {
         const double mode = clip(target_csl_mode);
-        gi = posneg(mode, 4.0, 16.0); /** TODO on gi change, reset z to correct value */
+        gi = posneg(mode, gi_pos, gi_neg);
         gf = target_csl_fb * pos(mode);
     }
 
     double step(double /*current position=*/p, double in = .0)
     {
-        update_mode();
+        if (p > limit_hi) { z = std::min(z, gi * limit_hi); in = std::min(.0, in); }
+        if (p < limit_lo) { z = std::max(z, gi * limit_lo); in = std::max(.0, in); }
 
-        if (p > limit_hi) z = std::min(z, gi * p);
-        if (p < limit_lo) z = std::max(z, gi * p);
-
-        double u = clip(-gi * p + z + in, .5);
+        double u = clip(-gi * p + z + gi*in*dt, .5);
+        update_mode(); /* change params here for z to have correct value */
         z = gi * p + gf * u;
 
         return u;
@@ -51,6 +58,7 @@ public:
         update_mode();
         z = gi * p; /* set initial conditions */
     }
+
 };
 
 } /* namespace supreme */
